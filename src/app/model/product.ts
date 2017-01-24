cd80299ea9144651713ef6aac71e3ebf4599d62a
Cédric Van Eerdenbrugghe <cedricve@gmail.com>
2017-01-24 09:12:06 +0100

    TP3

---
+++ src/app/model/product.ts
@@ -0,0 +1,11 @@
+export class Product {
+	// title: string;
+	// description: string;
+	// photo: string;
+	// price: number;
+	constructor(public title: string,
+				public description: string,
+				public photo: string,
+				public price: number){
+	}
+}
